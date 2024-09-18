39950e57ada23818afbe1917b2be7717cbf05e24
SuperDegz <joshuacarl.deguzman.eng@ust.edu.ph>
2024-09-18 22:54:22 +0800

    Add files via upload

---
+++ De Guzman_Pandas-P1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Problem 1:
+
+# In[16]:
+
+
+import pandas as pd
+
+
+# In[36]:
+
+
+df = pd.read_csv('cars.csv')
+df
+
+
+# In[34]:
+
+
+print("First five rows of the DataFrame:")
+print(cars.head()) 
+
+print("\nLast Five rows of the DataFrame:")
+print(cars.tail())
+
+
+# In[ ]:
+
+
+
+
